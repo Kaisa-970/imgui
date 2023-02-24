725dac6adbd5549ce0903e13d2c33160dcc68d77
闫鹏凯 <yanpk@seer-group.com>
2023-02-24 08:24:54 +0800

    change premake source file

--- premake5.lua
+++ premake5.lua
@@ -7,22 +7,8 @@ project "GLFW"
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
-		"src/platform.c",
-		"src/vulkan.c",
-		"src/window.c",
+		"*.h",
+		"*.cpp"
 	}
 
 		pic "On"
